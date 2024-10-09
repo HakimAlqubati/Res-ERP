a5f50cb5da719727fcbb4528aa5886d26755ae7d
Hakeem Qubati <hakimahmed123321@gmail.com>
2024-10-09 18:36:47 +0300

    fix issue

---
+++ app/Http/Middleware/CheckAuthenticated.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckAuthenticated
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle($request, Closure $next)
+    {
+        // Check if the user is authenticated
+        if (!Auth::check()) {
+            // If the request expects JSON, return a custom message
+            if ($request->expectsJson()) {
+                return response()->json(['message' => 'Unauthorized access. Please log in to continue.'], 401);
+            }
+            
+            // Otherwise, redirect to the custom login page
+            return redirect('/admin/login');
+        }
+
+        return $next($request);
+    }
+}
