099cc27d2d05f1e3dbb0085a164f57fc5a88ea67
HakimAlqubati <hakimahmed123321@gmail.com>
2024-10-27 23:10:49 +0300

    validation to disallow repeates applications

--- app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource/Pages/CreateEmployeeApplication.php
+++ app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource/Pages/CreateEmployeeApplication.php
@@ -5,8 +5,11 @@ namespace App\Filament\Clusters\HRApplicationsCluster\Resources\EmployeeApplicat
 use App\Filament\Clusters\HRApplicationsCluster\Resources\EmployeeApplicationResource;
 use App\Models\Employee;
 use App\Models\EmployeeApplication;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Validation\ValidationException;
 
 class CreateEmployeeApplication extends CreateRecord
 {
@@ -14,6 +17,28 @@ class CreateEmployeeApplication extends CreateRecord
     protected static string $resource = EmployeeApplicationResource::class;
     protected function mutateFormDataBeforeCreate(array $data): array
     {
+        $applicationType = EmployeeApplication::APPLICATION_TYPES[$data['application_type']];
+        // Log::warning('An application already exists for this employee on the selected date.');
+        
+        $data['application_type_id'] = $data['application_type'];
+        $data['application_type_name'] = $applicationType;
+        // dd($data);
+        // Check if an application already exists for the same employee and date
+        $existingApplication = EmployeeApplication::where('employee_id', $data['employee_id'])
+            ->where('application_date', $data['application_date'])
+            ->where('application_type_id', $data['application_type_id'])
+            ->where('status',EmployeeApplication::STATUS_APPROVED)
+            ->first();
+
+            if ($existingApplication) {
+                Notification::make()->body('An application already exists for this employee on the selected date.')->warning()->send();
+                Log::warning('An application already exists for this employee on the selected date.');
+                // Throw a validation exception if an application exists
+                throw ValidationException::withMessages([
+                    'application_date' => 'An application already exists for this employee on the selected date.',
+                ]);
+            }
+            
         if (!isStuff()) {
             $employee = Employee::find($data['employee_id']);
             if ($employee->branch()->exists()) {
@@ -25,7 +50,6 @@ class CreateEmployeeApplication extends CreateRecord
             $data['employee_id'] = auth()->user()->employee->id;
             $data['branch_id'] = auth()->user()->branch_id;
         }
-        $applicationType = EmployeeApplication::APPLICATION_TYPES[$data['application_type']];
 
         $data['application_type_id'] = $data['application_type'];
         $data['application_type_name'] = $applicationType;
