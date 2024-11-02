c6794d2ae58a9e05be239551742f500e6fb53195
HakimAlqubati <hakimahmed123321@gmail.com>
2024-11-02 11:51:38 +0300

    Update EmployeeResource.php

--- app/Filament/Resources/EmployeeResource.php
+++ app/Filament/Resources/EmployeeResource.php
@@ -332,7 +332,7 @@ class EmployeeResource extends Resource
                 ImageColumn::make('avatar_image')->label('')
                     ->circular(),
                 TextColumn::make('id')->label('id')->copyable(),
-                TextColumn::make('avatar_image2')->copyable()->label('avatar name')
+                TextColumn::make('avatar_image2')->copyable()->label('avatar name')->hidden()
                    ,
                 TextColumn::make('employee_no')
                     ->toggleable(isToggledHiddenByDefault: true)
