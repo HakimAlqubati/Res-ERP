e14638c535025d41a8bee82ea57fc2ad998206c6
HakimAlqubati <hakimahmed123321@gmail.com>
2024-11-09 04:47:11 +0300

    Update SettingResource.php

--- app/Filament/Resources/SettingResource.php
+++ app/Filament/Resources/SettingResource.php
@@ -45,7 +45,7 @@ class SettingResource extends Resource
                                     TextInput::make("company_name")
                                     ->label('Name'),
                                     TextInput::make("company_phone")
-                                    ->label('Name')
+                                    ->label('Phone')
                                     
                                     ->required(),
                                 FileUpload::make('company_logo')
