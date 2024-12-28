eca5e1e05c250cb4935dbebde7f1671d66f94066
HakimAlqubati <hakimahmed123321@gmail.com>
2024-12-28 23:02:21 +0300

    Update EmployeeApplicationV2.php

--- app/Models/EmployeeApplicationV2.php
+++ app/Models/EmployeeApplicationV2.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Traits\DynamicConnection;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -9,7 +10,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class EmployeeApplicationV2 extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes,DynamicConnection;
 
     protected $appends = [
         'detailed_leave_request',
