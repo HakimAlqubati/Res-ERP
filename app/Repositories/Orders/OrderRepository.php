096074cf8db99665967ec8d48e9f231783fc7a9a
HakimAlqubati <hakimahmed123321@gmail.com>
2025-03-17 01:38:35 +0300

    Update OrderRepository.php

--- app/Repositories/Orders/OrderRepository.php
+++ app/Repositories/Orders/OrderRepository.php
@@ -144,7 +144,7 @@ class OrderRepository implements OrderRepositoryInterface
             }
 
             // $orderDetailsData_old = calculateFifoMethod($request->input('order_details'), $orderId);
-            // dd($orderDetailsData, $orderDetailsData_old);
+            dd($orderDetailsData);
             $orderDetailsData = array_map(function ($item) {
                 unset(
                     $item['movement_date'],
