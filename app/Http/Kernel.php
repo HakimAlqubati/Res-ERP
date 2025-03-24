a10a62d3e8e76d805482afce0225d56c461db1b9
HakimAlqubati <hakimahmed123321@gmail.com>
2025-03-24 13:03:53 +0300

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -37,9 +37,7 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\CheckAuthenticated::class,
-            \App\Http\Middleware\SetTenantAppUrl::class,
-
+            \App\Http\Middleware\CheckAuthenticated::class, 
         ],
 
         'api' => [
