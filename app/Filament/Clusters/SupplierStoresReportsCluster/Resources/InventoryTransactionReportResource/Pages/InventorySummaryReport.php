c52c3c081ef41166241f7ce31dfce3423f2a13fd
HakimAlqubati <hakimahmed123321@gmail.com>
2025-04-21 22:53:39 +0300

    fix issues

--- app/Filament/Clusters/SupplierStoresReportsCluster/Resources/InventoryTransactionReportResource/Pages/InventorySummaryReport.php
+++ app/Filament/Clusters/SupplierStoresReportsCluster/Resources/InventoryTransactionReportResource/Pages/InventorySummaryReport.php
@@ -67,10 +67,10 @@ class InventorySummaryReport extends Page
                 $orderedQtyRes = $filteredOrderQuantities['quantity'] ?? 0;
                 $calculatedStock = $openingBalanceResult - ($orderedQtyRes + $row['remaining_qty']);
                 $calculatedStock = round($calculatedStock, 2);
-                if ($calculatedStock >= 0.01 && $calculatedStock <= 0.03) {
+                // if ($calculatedStock >= 0.01 && $calculatedStock <= 0.03) {
+                if (abs($calculatedStock) < 0.00001) {
                     $calculatedStock = 0;
                 }
-
                 if ($calculatedStock == 0 && $this->showWithoutZero == 1) {
                     continue;
                 }
