9970ccef050182365fb1a24085c3cdc4d8a2d09e
HakimAlqubati <hakimahmed123321@gmail.com>
2025-04-23 12:48:12 -0700

    Update OrderRepository.php

--- app/Repositories/Orders/OrderRepository.php
+++ app/Repositories/Orders/OrderRepository.php
@@ -102,8 +102,7 @@ class OrderRepository implements OrderRepositoryInterface
 
         if (
             isBranchUser() && isset(auth()->user()->branch)
-        ) {
-            dd(auth()->user()->owner->id,auth()->user()->branch_id);
+        ) { 
             $query->where('customer_id', auth()->user()->owner->id)
             ->where('branch_id',auth()->user()->branch_id)
             ;
