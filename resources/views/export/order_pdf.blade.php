150d018612c526b29c97e99b583dfd2fa195b27a
HakimAlqubati <hakimahmed123321@gmail.com>
2025-04-24 13:31:30 +0300

    Update order_pdf.blade.php

--- resources/views/export/order_pdf.blade.php
+++ resources/views/export/order_pdf.blade.php
@@ -112,8 +112,8 @@
                 <th> {{ __('lang.product') }} </th>
                 <th> {{ __('lang.unit') }} </th>
                 <th> {{ __('lang.quantity') }} </th>
-                {{-- <th> {{ __('lang.unit_price') }} </th> --}}
-                {{-- <th> {{ __('lang.total_price') }} </th> --}}
+                <th> {{ __('lang.unit_price') }} </th>
+                <th> {{ __('lang.total_price') }} </th>
             </tr>
 
         </thead>
@@ -134,8 +134,8 @@
                     <td> {{ $valDetail->product->name }} </td>
                     <td> {{ $valDetail->unit->name }} </td>
                     <td> {{ $valDetail->available_quantity }} </td>
-                    {{-- <td> {{ $valDetail->price }} </td> --}}
-                    {{-- <td> {{ $valDetail->price * $valDetail->available_quantity }} </td> --}}
+                    <td> {{ $valDetail->price }} </td>
+                    <td> {{ $valDetail->price * $valDetail->available_quantity }} </td>
                 </tr>
             @endforeach
         </tbody>
@@ -146,8 +146,8 @@
                     {{ __('lang.total') }}
                 </th>
                 <th> {{ $totalQty }} </th>
-                {{-- <th> {{ $totalPrice }} </th>
-                <th> {{ $totalPrices }} </th> --}}
+                <th> {{ $totalPrice }} </th>
+                <th> {{ $totalPrices }} </th>
             </tr>
         </tfoot>
     </table>
