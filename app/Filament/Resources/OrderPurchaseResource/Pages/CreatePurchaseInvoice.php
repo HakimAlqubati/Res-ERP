f9b9b6c4d173a8a1201308ae76c6a81ab67796cf
HakimAlqubati <hakimahmed123321@gmail.com>
2025-05-13 01:07:56 +0300

    add product_id, unit_id as unique in purchase invoice details

--- app/Filament/Resources/OrderPurchaseResource/Pages/CreatePurchaseInvoice.php
+++ app/Filament/Resources/OrderPurchaseResource/Pages/CreatePurchaseInvoice.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\PurchaseInvoiceResource\Pages;
 
 use App\Filament\Resources\PurchaseInvoiceResource;
+use Filament\Notifications\Notification;
 use Filament\Pages\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Validation\ValidationException;
 
 class CreatePurchaseInvoice extends CreateRecord
 {
@@ -16,6 +18,31 @@ class CreatePurchaseInvoice extends CreateRecord
         return $data;
     }
 
+    public function create(bool $another = false): void
+    {
+        try {
+            parent::create($another);
+        } catch (\Illuminate\Database\QueryException $e) {
+            Notification::make()
+                ->title('Database Error')
+                ->body($e->getMessage())
+                ->danger()
+                ->send();
+        } catch (ValidationException $e) {
+            // This is already handled by Filament normally, but optional
+            Notification::make()
+                ->title('Validation Failed')
+                ->body('Please check your inputs.')
+                ->warning()
+                ->send();
+        } catch (\Throwable $e) {
+            Notification::make()
+                ->title('Unexpected Error')
+                ->body($e->getMessage())
+                ->danger()
+                ->send();
+        }
+    }
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
