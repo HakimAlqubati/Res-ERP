6ff896056bedf5ca68a1184b53a7fb1ea73e17ed
HakimAlqubati <hakimahmed123321@gmail.com>
2025-06-14 17:55:19 +0300

    Update ProductRepository.php

--- app/Repositories/Products/ProductRepository.php
+++ app/Repositories/Products/ProductRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repositories\Products;
 
+use App\Filament\Resources\OrderReportsResource\GeneralReportOfProductsResource;
 use App\Http\Resources\ProductResource;
 use App\Interfaces\Products\ProductRepositoryInterface;
 use App\Models\Branch;
@@ -51,7 +52,7 @@ class ProductRepository implements ProductRepositoryInterface
             ->when($code, function ($query) use ($code) {
                 return $query->where('code', $code);
             })
-            
+
             ->when($categoryId, function ($query) use ($categoryId) {
                 return $query->where('category_id', $categoryId);
             });
@@ -162,57 +163,58 @@ class ProductRepository implements ProductRepositoryInterface
         $month = $request->input('month');
         $product_id = $request->input('product_id');
 
-        $data = DB::table('orders_details')
-            ->join('orders', 'orders_details.order_id', '=', 'orders.id')
-            ->join('products', 'orders_details.product_id', '=', 'products.id')
-            ->select(
-                'products.category_id',
-                DB::raw('SUM(orders_details.available_quantity) as available_quantity'),
-                DB::raw('SUM(orders_details.price) as price')
-            )
-            ->when($product_id, function ($query) use ($product_id) {
-                return $query->where('orders_details.product_id', $product_id);
-            })
-            ->when($branch_id, function ($query) use ($branch_id) {
-                return $query->where('orders.branch_id', $branch_id);
-            })
-            ->when($from_date && $to_date, function ($query) use ($from_date, $to_date) {
-                return $query->whereBetween('orders.created_at', [$from_date, $to_date]);
-            })
-            ->when($year && $month, function ($query) use ($year, $month) {
-                return $query->whereRaw('YEAR(orders.created_at) = ? AND MONTH(orders.created_at) = ?', [$year, $month]);
-            })
-            // ->whereIn('orders.status', [Order::DELEVIRED, Order::READY_FOR_DELEVIRY])
-            ->where('orders.active', 1)
-            ->whereNull('orders.deleted_at')
-            ->groupBy('products.category_id')
-            ->get()
-            ->mapWithKeys(function ($item) {
-                if (is_object($item)) {
-                    return [$item->category_id => [
-                        'available_quantity' => $item->available_quantity,
-                        'price' => $item->price
-                    ]];
-                }
-            })
-            ->all();
-        $categories = DB::table('categories')->where('active', 1)->get(['id', 'name'])->pluck('name', 'id');
-
-        foreach ($categories as $cat_id => $cat_name) {
-            $obj = new \stdClass();
-            $obj->category_id = $cat_id;
-            $obj->category_name = $cat_name;
-            $obj->available_quantity =  round(isset($data[$cat_id]) ? $data[$cat_id]['available_quantity'] : 0, 0);
-            $price = (isset($data[$cat_id]) ? $data[$cat_id]['price'] : '0.00');
-            $obj->price =  formatMoney($price, $this->currency);
-            $obj->amount = number_format($price, 2);
-            $obj->symbol = $this->currency;
-            $final_result[] = $obj;
-        }
+        $data =  GeneralReportOfProductsResource::processReportData($from_date, $to_date, $branch_id);
+        // $data = DB::table('orders_details')
+        //     ->join('orders', 'orders_details.order_id', '=', 'orders.id')
+        //     ->join('products', 'orders_details.product_id', '=', 'products.id')
+        //     ->select(
+        //         'products.category_id',
+        //         DB::raw('SUM(orders_details.available_quantity) as available_quantity'),
+        //         DB::raw('SUM(orders_details.price) as price')
+        //     )
+        //     ->when($product_id, function ($query) use ($product_id) {
+        //         return $query->where('orders_details.product_id', $product_id);
+        //     })
+        //     ->when($branch_id, function ($query) use ($branch_id) {
+        //         return $query->where('orders.branch_id', $branch_id);
+        //     })
+        //     ->when($from_date && $to_date, function ($query) use ($from_date, $to_date) {
+        //         return $query->whereBetween('orders.created_at', [$from_date, $to_date]);
+        //     })
+        //     ->when($year && $month, function ($query) use ($year, $month) {
+        //         return $query->whereRaw('YEAR(orders.created_at) = ? AND MONTH(orders.created_at) = ?', [$year, $month]);
+        //     })
+        //     // ->whereIn('orders.status', [Order::DELEVIRED, Order::READY_FOR_DELEVIRY])
+        //     ->where('orders.active', 1)
+        //     ->whereNull('orders.deleted_at')
+        //     ->groupBy('products.category_id')
+        //     ->get()
+        //     ->mapWithKeys(function ($item) {
+        //         if (is_object($item)) {
+        //             return [$item->category_id => [
+        //                 'available_quantity' => $item->available_quantity,
+        //                 'price' => $item->price
+        //             ]];
+        //         }
+        //     })
+        //     ->all();
+        // $categories = DB::table('categories')->where('active', 1)->get(['id', 'name'])->pluck('name', 'id');
+        
+        // foreach ($categories as $cat_id => $cat_name) {
+        //     $obj = new \stdClass();
+        //     $obj->category_id = $cat_id;
+        //     $obj->category_name = $cat_name;
+        //     $obj->available_quantity =  round(isset($data[$cat_id]) ? $data[$cat_id]['available_quantity'] : 0, 0);
+        //     $price = (isset($data[$cat_id]) ? $data[$cat_id]['price'] : '0.00');
+        //     $obj->price =  formatMoney($price, $this->currency);
+        //     $obj->amount = number_format($price, 2);
+        //     $obj->symbol = $this->currency;
+        //     $final_result[] = $obj;
+        // }
 
         return [
             'branches' => Branch::where('active', 1)->pluck('name', 'id'),
-            'data' => $final_result
+            'data' => $data
         ];
     }
 
