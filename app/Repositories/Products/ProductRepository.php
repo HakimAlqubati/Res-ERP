b81aaacdca8a1398523f0984090eac94634f35f8
HakimAlqubati <hakimahmed123321@gmail.com>
2025-06-15 16:30:38 +0300

    Update ProductRepository.php

--- app/Repositories/Products/ProductRepository.php
+++ app/Repositories/Products/ProductRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories\Products;
 
 use App\Filament\Resources\OrderReportsResource\GeneralReportOfProductsResource;
+use App\Filament\Resources\OrderReportsResource\Pages\GeneralReportProductDetails;
 use App\Http\Resources\ProductResource;
 use App\Interfaces\Products\ProductRepositoryInterface;
 use App\Models\Branch;
@@ -233,57 +234,60 @@ class ProductRepository implements ProductRepositoryInterface
         $month = $request->input('month');
         $product_id = $request->input('product_id');
 
-        $data = DB::table('orders_details')
-            ->join('orders', 'orders_details.order_id', '=', 'orders.id')
-            ->join('products', 'orders_details.product_id', '=', 'products.id')
-            ->join('units', 'orders_details.unit_id', '=', 'units.id')
-            // ->select('products.category_id', 'orders_details.product_id as p_id' )
-            ->when($branch_id, function ($query) use ($branch_id) {
-                return $query->where('orders.branch_id', $branch_id);
-            })
-            ->when($product_id, function ($query) use ($product_id) {
-                return $query->where('orders_details.product_id', $product_id);
-            })
-            ->when($from_date && $to_date, function ($query) use ($from_date, $to_date) {
-                return $query->whereBetween('orders.created_at', [$from_date, $to_date]);
-            })->when($year && $month, function ($query) use ($year, $month) {
-                return $query->whereRaw('YEAR(orders.created_at) = ? AND MONTH(orders.created_at) = ?', [$year, $month]);
-            })
-            // ->whereIn('orders.status', [Order::DELEVIRED, Order::READY_FOR_DELEVIRY])
-            ->where('orders.active', 1)
-            ->whereNull('orders.deleted_at')
-            ->where('products.category_id', $category_id)
-            ->groupBy(
-                'orders_details.product_id',
-                'products.category_id',
-                'orders_details.unit_id',
-                'products.name',
-                'units.name',
-            )
-            ->get([
-                'products.category_id',
-                'orders_details.product_id',
-                DB::raw("IF(JSON_VALID(products.name), REPLACE(JSON_EXTRACT(products.name, '$." . app()->getLocale() . "'), '\"', ''), products.name) as product_name"),
-                'units.name as unit_name',
-                'orders_details.unit_id as unit_id',
-                DB::raw('ROUND(SUM(orders_details.available_quantity), 0) as available_quantity'),
-                DB::raw('(SUM(orders_details.price)) as price'),
-            ]);
-        $final_result = [];
-        foreach ($data as   $val_data) {
-            $obj = new \stdClass();
-            $obj->category_id = $val_data->category_id;
-            $obj->product_id = $val_data->product_id;
-            $obj->product_name = $val_data->product_name;
-            $obj->unit_name = $val_data->unit_name;
-            $obj->unit_id = $val_data->unit_id;
-            $obj->available_quantity = $val_data->available_quantity;
-            $obj->price = formatMoney($val_data->price, $this->currency);
-            $obj->amount = number_format($val_data->price, 2);
-            $obj->symbol = $this->currency;
-            $final_result[] = $obj;
-        }
-        return $final_result;
+
+        $reportData = (new GeneralReportProductDetails())->getReportDetails($from_date, $to_date, $branch_id, $category_id);
+        return $reportData;
+        // $data = DB::table('orders_details')
+        //     ->join('orders', 'orders_details.order_id', '=', 'orders.id')
+        //     ->join('products', 'orders_details.product_id', '=', 'products.id')
+        //     ->join('units', 'orders_details.unit_id', '=', 'units.id')
+        //     // ->select('products.category_id', 'orders_details.product_id as p_id' )
+        //     ->when($branch_id, function ($query) use ($branch_id) {
+        //         return $query->where('orders.branch_id', $branch_id);
+        //     })
+        //     ->when($product_id, function ($query) use ($product_id) {
+        //         return $query->where('orders_details.product_id', $product_id);
+        //     })
+        //     ->when($from_date && $to_date, function ($query) use ($from_date, $to_date) {
+        //         return $query->whereBetween('orders.created_at', [$from_date, $to_date]);
+        //     })->when($year && $month, function ($query) use ($year, $month) {
+        //         return $query->whereRaw('YEAR(orders.created_at) = ? AND MONTH(orders.created_at) = ?', [$year, $month]);
+        //     })
+        //     // ->whereIn('orders.status', [Order::DELEVIRED, Order::READY_FOR_DELEVIRY])
+        //     ->where('orders.active', 1)
+        //     ->whereNull('orders.deleted_at')
+        //     ->where('products.category_id', $category_id)
+        //     ->groupBy(
+        //         'orders_details.product_id',
+        //         'products.category_id',
+        //         'orders_details.unit_id',
+        //         'products.name',
+        //         'units.name',
+        //     )
+        //     ->get([
+        //         'products.category_id',
+        //         'orders_details.product_id',
+        //         DB::raw("IF(JSON_VALID(products.name), REPLACE(JSON_EXTRACT(products.name, '$." . app()->getLocale() . "'), '\"', ''), products.name) as product_name"),
+        //         'units.name as unit_name',
+        //         'orders_details.unit_id as unit_id',
+        //         DB::raw('ROUND(SUM(orders_details.available_quantity), 0) as available_quantity'),
+        //         DB::raw('(SUM(orders_details.price)) as price'),
+        //     ]);
+        // $final_result = [];
+        // foreach ($data as   $val_data) {
+        //     $obj = new \stdClass();
+        //     $obj->category_id = $val_data->category_id;
+        //     $obj->product_id = $val_data->product_id;
+        //     $obj->product_name = $val_data->product_name;
+        //     $obj->unit_name = $val_data->unit_name;
+        //     $obj->unit_id = $val_data->unit_id;
+        //     $obj->available_quantity = $val_data->available_quantity;
+        //     $obj->price = formatMoney($val_data->price, $this->currency);
+        //     $obj->amount = number_format($val_data->price, 2);
+        //     $obj->symbol = $this->currency;
+        //     $final_result[] = $obj;
+        // }
+        // return $final_result;
     }
 
     public function getProductsOrdersQuntities($request)
