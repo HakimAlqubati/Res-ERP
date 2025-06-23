da641609e513abec21e47146d86dccccc3d51bed
HakimAlqubati <hakimahmed123321@gmail.com>
2025-06-24 00:35:09 +0300

    Create 2025_06_24_053132_update_total_price_after_waste_precision.php

---
+++ database/migrations/2025_06_24_053132_update_total_price_after_waste_precision.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product_items', function (Blueprint $table) {
+            $table->decimal('total_price_after_waste', 20, 10)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product_items', function (Blueprint $table) {
+            $table->decimal('total_price_after_waste', 10, 8)->change();
+        });
+    }
+};
