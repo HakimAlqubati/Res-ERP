87012a6c1eee45d66a3f2084a1d67a2fa3356ebb
HakimAlqubati <hakimahmed123321@gmail.com>
2025-07-05 21:27:33 +0300

    Update landing.php

--- routes/landing.php
+++ routes/landing.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Landing\WorkbenchController;
 
-Route::get('/workbench-erp', [WorkbenchController::class, 'show'])->name('landing.workbench');
-Route::get('/restaurant-erp', [WorkbenchController::class, 'restaurantErp'])->name('landing.restaurant-erp');
-Route::get('/faq', [WorkbenchController::class, 'faq'])->name('landing.faq');
+// Route::get('/workbench-erp', [WorkbenchController::class, 'show'])->name('landing.workbench');
+// Route::get('/restaurant-erp', [WorkbenchController::class, 'restaurantErp'])->name('landing.restaurant-erp');
+// Route::get('/faq', [WorkbenchController::class, 'faq'])->name('landing.faq');
