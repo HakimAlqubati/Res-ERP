71e95135ac09f57a92a068159b4e3f644af33fd9
HakimAlqubati <hakimahmed123321@gmail.com>
2025-07-26 16:18:24 +0300

    Update PurchaseInvoiceController.php

--- app/Http/Controllers/Api/PurchaseInvoiceController.php
+++ app/Http/Controllers/Api/PurchaseInvoiceController.php
@@ -54,6 +54,7 @@ class PurchaseInvoiceController extends Controller
                 return response()->json(['success' => false, 'message' => 'Invalid to_date format. Use d-m-Y.']);
             }
         }
+        $totalAmount = $query->sum('total_amount');
         // $invoices = $query->latest()->get();
         $paginator = $query->latest()->paginate($perPage);
         // 🧾 Format output
@@ -77,6 +78,7 @@ class PurchaseInvoiceController extends Controller
             'current_page' => $paginator->currentPage(),
             'per_page' => $paginator->perPage(),
             'total' => $paginator->total(),
+            'total_amount' => formatMoneyWithCurrency($totalAmount),
         ]);
     }
 }
