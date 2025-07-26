58b5f5accaf89f168a3419436485ede7d468124f
HakimAlqubati <hakimahmed123321@gmail.com>
2025-07-26 22:11:07 +0300

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,9 +21,9 @@ return Application::configure(basePath: dirname(__DIR__))
             ])
             ->web(append: [
                 \App\Http\Middleware\TrackUserLastActivity::class,
-                \App\Http\Middleware\LogUserActivity::class,
+                // \App\Http\Middleware\LogUserActivity::class,
             ])->api(append: [
-                \App\Http\Middleware\LogUserActivity::class,
+                // \App\Http\Middleware\LogUserActivity::class,
             ])
         ;
         $middleware->alias([
