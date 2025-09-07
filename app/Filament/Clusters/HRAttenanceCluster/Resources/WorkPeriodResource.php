cd5512e020cc3e1cb3238f96e8bcd14afa0c7af7
HakimAlqubati <hakimahmed123321@gmail.com>
2025-09-07 23:52:47 +0300

    Update WorkPeriodResource.php

--- app/Filament/Clusters/HRAttenanceCluster/Resources/WorkPeriodResource.php
+++ app/Filament/Clusters/HRAttenanceCluster/Resources/WorkPeriodResource.php
@@ -60,7 +60,7 @@ class WorkPeriodResource extends Resource
         return [
 
             Fieldset::make()->columnSpanFull()->schema([
-                Grid::make()->columnSpanFull()->columns(3)->schema([
+                Grid::make()->columns(3)->columnSpanFull()->schema([
                     TextInput::make('name')
                         ->label('Name')
                         ->required()
@@ -108,21 +108,21 @@ class WorkPeriodResource extends Resource
                         ->default('12:00:00'),
                 ]),
 
-                Grid::make()->columnSpanFull()->columns(2)->schema([
-                    Select::make('days')
-                        ->label('Days')
-                        ->multiple()
-                        ->options([
-                            'Monday' => 'Monday',
-                            'Tuesday' => 'Tuesday',
-                            'Wednesday' => 'Wednesday',
-                            'Thursday' => 'Thursday',
-                            'Friday' => 'Friday',
-                            'Saturday' => 'Saturday',
-                            'Sunday' => 'Sunday',
-                        ])->default(['Sunday'])
-                        ->columnSpan(1)
-                        ->required(),
+                // Grid::make()->columns(2)->schema([
+                //     Forms\Components\Select::make('days')
+                //         ->label('Days')
+                //         ->multiple()
+                //         ->options([
+                //             'Monday' => 'Monday',
+                //             'Tuesday' => 'Tuesday',
+                //             'Wednesday' => 'Wednesday',
+                //             'Thursday' => 'Thursday',
+                //             'Friday' => 'Friday',
+                //             'Saturday' => 'Saturday',
+                //             'Sunday' => 'Sunday',
+                //         ])->default(['Sunday'])
+                //         ->columnSpan(1)
+                //         ->required(),
 
                 //     // Forms\Components\TextInput::make('allowed_count_minutes_late')
                 //     //     ->label('Allowed Delay (Minutes)')->required()->default(0)
@@ -212,8 +212,8 @@ class WorkPeriodResource extends Resource
                                     ->default($record->end_at),
                                 Forms\Components\Select::make('branch_id')
                                     ->options(Branch::active()
-                                    ->branches()
-                                    ->pluck('name', 'id'))
+                                        ->branches()
+                                        ->pluck('name', 'id'))
                                     ->label('Branch')
                                     ->default($record->branch_id),
                                 // Forms\Components\TextInput::make('allowed_count_minutes_late')
