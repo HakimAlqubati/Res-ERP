8e8817d65f96eef4be83c6b7d1cd510391a725cb
HakimAlqubati <hakimahmed123321@gmail.com>
2025-09-13 16:19:59 +0300

    Update SalaryTransactionResource.php

--- app/Filament/Resources/SalaryTransactions/SalaryTransactionResource.php
+++ app/Filament/Resources/SalaryTransactions/SalaryTransactionResource.php
@@ -30,7 +30,7 @@ class SalaryTransactionResource extends Resource
     protected static ?string $cluster = HRSalaryCluster::class;
     protected static ?\Filament\Pages\Enums\SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
 
-    protected static ?int $navigationSort = 15;
+    protected static ?int $navigationSort = 2;
     public static function form(Schema $schema): Schema
     {
         return SalaryTransactionForm::configure($schema);
