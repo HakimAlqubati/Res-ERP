088b4d8ad9c856ea459a98dd41091d7fa53c1832
HakimAlqubati <hakimahmed123321@gmail.com>
2025-09-18 01:25:01 +0300

    Update Attendance.php

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use Carbon\Carbon;
+ use Illuminate\Database\Eloquent\Builder;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
