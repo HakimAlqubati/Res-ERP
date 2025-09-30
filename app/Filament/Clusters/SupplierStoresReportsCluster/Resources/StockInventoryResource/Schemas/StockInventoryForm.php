816902d163bb5392d9a767350e34a254b8616e4d
HakimAlqubati <hakimahmed123321@gmail.com>
2025-09-30 14:19:56 +0300

    Update StockInventoryForm.php

--- app/Filament/Clusters/SupplierStoresReportsCluster/Resources/StockInventoryResource/Schemas/StockInventoryForm.php
+++ app/Filament/Clusters/SupplierStoresReportsCluster/Resources/StockInventoryResource/Schemas/StockInventoryForm.php
@@ -141,7 +141,7 @@ class StockInventoryForm
                                             })->toArray();
 
                                             // ⚠️ مؤقتًا: قص العدد الظاهر لتخفيف الـDOM أثناء التشخيص
-                                            $details = array_slice($details, 51, 105);
+                                            $details = array_slice($details, 51, 100);
 
                                             $set('details', $details);
 
