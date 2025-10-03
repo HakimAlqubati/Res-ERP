e7d46ca32d610c739d616b745928677cac711a80
HakimAlqubati <hakimahmed123321@gmail.com>
2025-10-03 03:16:56 +0300

    Update Employee.php

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -147,9 +147,9 @@ class Employee extends Model implements Auditable
     public function getAvatarImageAttribute()
     {
         // Check if avatar is set and exists on S3
-        // if ($this->avatar && Storage::disk('s3')->exists($this->avatar)) {
-        //     return Storage::disk('s3')->url($this->avatar);
-        // }
+        if ($this->avatar && Storage::disk('s3')->exists($this->avatar)) {
+            return Storage::disk('s3')->url($this->avatar);
+        }
         if ($this->avatar && Storage::disk('public')->exists($this->avatar)) {
             if (env('APP_ENV') == 'local') {
                 return Storage::disk('public')->url($this->avatar);
