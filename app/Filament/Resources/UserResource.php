5b97322f8f555b3235c5b48a827221adab586303
HakimAlqubati <hakimahmed123321@gmail.com>
2025-10-10 04:19:06 +0300

    Update UserResource.php

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -3,54 +3,21 @@
 namespace App\Filament\Resources;
 
 use Filament\Pages\Enums\SubNavigationPosition;
-use Filament\Schemas\Schema;
-use Filament\Tables\Filters\Filter;
-use Filament\Tables\Filters\TrashedFilter;
-use Filament\Tables\Filters\SelectFilter;
-use Filament\Actions\ActionGroup;
-use Filament\Actions\EditAction;
-use Filament\Actions\DeleteAction;
-use Filament\Actions\RestoreAction;
-use Filament\Actions\Action;
-use Filament\Schemas\Components\Fieldset;
-use Throwable;
-use Exception;
-use Filament\Actions\DeleteBulkAction;
-use Filament\Actions\ForceDeleteBulkAction;
-use Filament\Actions\RestoreBulkAction;
+use Filament\Schemas\Schema;  
 use App\Filament\Resources\UserResource\Pages\ListUsers;
 use App\Filament\Resources\UserResource\Pages\CreateUser;
-use App\Filament\Resources\UserResource\Pages\EditUser;
-use App\Filament\Resources\UserResource\Pages;
-use App\Filament\Resources\UserResource\Pages\UserTable;
-use App\Filament\Tables\Columns\SoftDeleteColumn;
+use App\Filament\Resources\UserResource\Pages\EditUser; 
+use App\Filament\Resources\UserResource\Pages\UserTable; 
 use App\Filament\Traits\Forms\HasAttendanceForm;
 use App\Filament\Traits\Forms\HasEmployeeExistingForm;
-use App\Filament\Traits\Forms\HasNewUserForm;
-use App\Models\Branch;
-use App\Models\LoginAttempt;
-use App\Models\User;
-use Filament\Forms\Components\Hidden;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
+use App\Filament\Traits\Forms\HasNewUserForm; 
+use App\Models\User; 
 use Filament\Forms\Components\ToggleButtons;
 use Filament\Resources\Pages\Page;
-use Filament\Resources\Resource;
-use Filament\Support\Colors\Color;
-use Filament\Support\Enums\Alignment;
-use Filament\Tables;
-use Filament\Tables\Columns\IconColumn;
-use Filament\Tables\Columns\ImageColumn;
-use Filament\Tables\Columns\TextColumn;
+use Filament\Resources\Resource; 
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Log;
-use Illuminate\Validation\Rules\Password;
-use Spatie\Permission\Models\Role;
-use Ysfkaya\FilamentPhoneInput\Forms\PhoneInput;
-use Ysfkaya\FilamentPhoneInput\PhoneInputNumberType;
+use Illuminate\Database\Eloquent\SoftDeletingScope; 
 
 // use pxlrbt\FilamentExcel\Actions\Tables\ExportBulkAction;
 
@@ -87,16 +54,7 @@ class UserResource extends Resource
                     ])->visibleOn('create')
                     ->default('new_user')
                     ->live()
-                    ->required(),
-                // Fieldset::make()->label('')->columns(2)->schema([
-                //     Toggle::make('is_exist_employee')->label('')
-                //         ->inline(false)->helperText('Check if you want to create a user account for existing employee')
-                //         ->live(),
-                //     Toggle::make('is_attendance_user')->label('')
-                //         ->inline(false)->helperText('Check if you want to create a user account for attendance webcam')
-                //         ->live(),
-                // ])->hiddenOn('edit'),
-                // self::employeExistingForm(),
+                    ->required(), 
                 self::newUserForm(),
                 self::attendanceForm(),
             ]);
