37f254d84e9da77e30e0870c717dafc0c08309dd
HakimAlqubati <hakimahmed123321@gmail.com>
2025-11-04 00:42:50 +0300

    Update InventoryDashboardService.php

--- app/Services/Reports/InventoryDashboardService.php
+++ app/Services/Reports/InventoryDashboardService.php
@@ -99,6 +99,14 @@ class InventoryDashboardService
                         'month_to_date' => formatMoneyWithCurrency($monthToDateValue),
                     ],
                 ],
+                'chocolate2' => [
+                    'items_made' => $itemsMade,
+                    'value'      => [
+                        'today'         => formatMoneyWithCurrency($todayValue),
+                        'yesterday'     => formatMoneyWithCurrency($yesterdayValue),
+                        'month_to_date' => formatMoneyWithCurrency($monthToDateValue),
+                    ],
+                ],
             ] : [],
         ];
     }
