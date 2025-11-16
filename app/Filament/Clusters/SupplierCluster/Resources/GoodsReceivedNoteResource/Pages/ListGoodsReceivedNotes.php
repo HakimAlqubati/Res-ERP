32dddf4f57b8f8893e2d0c082dd00fda6fa08794
HakimAlqubati <hakimahmed123321@gmail.com>
2025-11-16 08:04:46 +0300

    Update ListGoodsReceivedNotes.php

--- app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource/Pages/ListGoodsReceivedNotes.php
+++ app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource/Pages/ListGoodsReceivedNotes.php
@@ -16,7 +16,7 @@ class ListGoodsReceivedNotes extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            // CreateAction::make(),
+            CreateAction::make(),
         ];
     }
 
