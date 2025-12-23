55a8667d7daf215608f35c07fb5a2843d6daac41
HakimAlqubati <hakimahmed123321@gmail.com>
2025-12-23 17:02:33 +0300

    Update app.php

--- config/app.php
+++ config/app.php
@@ -85,16 +85,16 @@ return [
     |
     */
 
-    'locale' => 'ar',
+    // 'locale' => 'ar',
 
-    'fallback_locale' => 'ar',
+    // 'fallback_locale' => 'ar',
 
-    'faker_locale' => 'ar_SA',
-    // 'locale' => env('APP_LOCALE', 'en'),
+    // 'faker_locale' => 'ar_SA',
+    'locale' => env('APP_LOCALE', 'en'),
 
-    // 'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    // 'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
     /*
     |--------------------------------------------------------------------------
