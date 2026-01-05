6d25ab31306d0817e3fb36090364019794ab8607
HakimAlqubati <hakimahmed123321@gmail.com>
2026-01-05 16:38:47 +0300

    perf: Remove slow getRemainingQty() from boot events + add inventory benchmark tools

--- routes/api_dev.php
+++ routes/api_dev.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\Developer\DevInventoryController;
 use App\Http\Controllers\Api\Developer\DevBenchmarkController;
+use App\Http\Controllers\Api\Developer\DevOrderController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,12 @@ Route::prefix('dev')->middleware('auth:api')->group(function () {
     Route::post('/stockSupply', [DevInventoryController::class, 'stockSupply']);
     Route::post('/stockIssue', [DevInventoryController::class, 'stockIssue']);
     Route::post('/randomPurchase', [DevInventoryController::class, 'randomPurchase']);
+
+    // Orders
+    Route::prefix('order')->group(function () {
+        Route::get('/{id}/details', [DevOrderController::class, 'details']);
+        Route::post('/{id}/moveInventory', [DevOrderController::class, 'moveInventory']);
+    });
 
     // Benchmark
     Route::prefix('benchmark')->group(function () {
