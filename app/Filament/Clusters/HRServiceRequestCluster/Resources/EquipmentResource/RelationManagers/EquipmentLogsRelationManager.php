789a5e59859d9d25131fff189ebdabe8af449585
HakimAlqubati <hakimahmed123321@gmail.com>
2026-02-01 17:42:39 +0300

    add equipment logs relation manager

---
+++ app/Filament/Clusters/HRServiceRequestCluster/Resources/EquipmentResource/RelationManagers/EquipmentLogsRelationManager.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Filament\Clusters\HRServiceRequestCluster\Resources\EquipmentResource\RelationManagers;
+
+use Filament\Actions\AssociateAction;
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\CreateAction;
+use Filament\Actions\DeleteAction;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DissociateAction;
+use Filament\Actions\DissociateBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Forms\Components\TextInput;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Schemas\Schema;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Model;
+
+class EquipmentLogsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'logs';
+
+    public static function getBadge(Model $ownerRecord, string $pageClass): ?string
+    { 
+        return $ownerRecord->logs()->count();
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->striped()
+            ->recordTitleAttribute('action')
+            ->columns([
+                TextColumn::make('action')
+                    ->searchable(),
+                TextColumn::make('performedBy.name')
+                    ->searchable(),
+                TextColumn::make('description')
+                    ->searchable(),
+                TextColumn::make('created_at')
+                    ->searchable()
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                CreateAction::make(),
+                AssociateAction::make(),
+            ])
+            ->recordActions([
+                EditAction::make(),
+                DissociateAction::make(),
+                DeleteAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DissociateBulkAction::make(),
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
