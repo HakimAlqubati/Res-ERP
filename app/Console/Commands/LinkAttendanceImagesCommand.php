e7d6971833640d36162d839fe1cc022cc514e90d
HakimAlqubati <hakimahmed123321@gmail.com>
2026-02-26 02:27:39 +0300

    Update LinkAttendanceImagesCommand.php

--- app/Console/Commands/LinkAttendanceImagesCommand.php
+++ app/Console/Commands/LinkAttendanceImagesCommand.php
@@ -53,7 +53,9 @@ class LinkAttendanceImagesCommand extends Command
             foreach ($attendances as $attendance) {
                 try {
                     // Combine date and time to get full timestamp (use real_check_date to handle night shifts)
-                    $actualDate = $attendance->real_check_date ?: $attendance->check_date;
+                    $actualDate = $attendance->real_check_date
+                        // ?: $attendance->check_date
+                    ;
                     $attendanceTimestamp = Carbon::parse($actualDate . ' ' . $attendance->check_time);
 
                     // Logic based on AttendanceContext.php:
