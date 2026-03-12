5c2d41fa322fe9af0d02af2f85c1760a61eda4dc
HakimAlqubati <hakimahmed123321@gmail.com>
2026-03-12 22:18:09 +0300

    Update ListEmployeeApplications.php

--- app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource/Pages/ListEmployeeApplications.php
+++ app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource/Pages/ListEmployeeApplications.php
@@ -39,14 +39,9 @@ class ListEmployeeApplications extends ListRecords
                 ->badgeColor('warning'),
             EmployeeApplicationV2::APPLICATION_TYPE_NAMES[EmployeeApplicationV2::APPLICATION_TYPE_ADVANCE_REQUEST] => Tab::make()
                 ->label(__('lang.advance_request'))
-                ->modifyQueryUsing(
-                    fn(Builder $query) => $query->where('application_type_id', EmployeeApplicationV2::APPLICATION_TYPE_ADVANCE_REQUEST)
-
-                    // ->with(['employee:id,name,employee_no,branch_id', 'employee.branch:id,name', 'application:id,status'])
-                )
+                ->modifyQueryUsing(fn(Builder $query) => $query->where('application_type_id', EmployeeApplicationV2::APPLICATION_TYPE_ADVANCE_REQUEST))
                 ->icon('heroicon-m-banknotes')
-                ->badge(EmployeeApplicationV2::query()->where('application_type_id', EmployeeApplicationV2::APPLICATION_TYPE_ADVANCE_REQUEST)
-                    ->count())
+                ->badge(EmployeeApplicationV2::query()->where('application_type_id', EmployeeApplicationV2::APPLICATION_TYPE_ADVANCE_REQUEST)->count())
                 ->badgeColor('warning'),
             EmployeeApplicationV2::APPLICATION_TYPE_NAMES[EmployeeApplicationV2::APPLICATION_TYPE_LEAVE_REQUEST] => Tab::make()
                 ->label(__('lang.leave_request'))
