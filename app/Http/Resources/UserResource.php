0cdc8fe4126f2e604ddce0c253c7a897b61533c9
HakimAlqubati <hakimahmed123321@gmail.com>
2026-03-12 20:59:23 +0300

    Update UserResource.php

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -20,6 +20,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
+            'avatar' => $this->avatar_image,
             'email' => $this->email,
             'owner_id' => $this->owner_id,
             'role_id' => $this->roles[0]->id,
