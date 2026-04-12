d123c7af7dd0886e3ab1a0996622b66db43557a8
HakimAlqubati <hakimahmed123321@gmail.com>
2026-04-12 08:42:19 +0300

    fix issues

--- app/Http/Resources/HR/Maintenance/EquipmentLogResource.php
+++ app/Http/Resources/HR/Maintenance/EquipmentLogResource.php
@@ -9,12 +9,14 @@ class EquipmentLogResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id'           => $this->id,
-            'equipment_id' => $this->equipment_id,
-            'action'       => $this->action,
-            'description'  => $this->description,
-            'performed_by' => $this->performed_by,
-            'created_at'   => $this->created_at,
+            'id'             => $this->id,
+            'equipment_id'   => $this->equipment_id,
+            'equipment_name' => $this->equipment?->name,
+            'action'         => $this->action,
+            'description'    => $this->description,
+            'performed_by'      => $this->performed_by,
+            'performed_by_name' => $this->performedBy?->name,
+            'created_at'        => $this->created_at,
         ];
     }
 }
