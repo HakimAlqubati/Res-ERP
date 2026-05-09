70c461e0ec5007dbf05291585ffe3bbf6e9ecc9f
HakimAlqubati <hakimahmed123321@gmail.com>
2026-05-09 10:40:53 +0300

    Update BranchScope.php

--- app/Traits/Scopes/BranchScope.php
+++ app/Traits/Scopes/BranchScope.php
@@ -61,7 +61,7 @@ trait BranchScope
         if (auth()->check() && (isSuperAdmin() || isSystemManager())) {
             return $query;
         }
-        if (auth()->check() && isStuff()) {
+        if (auth()->check() && (isStuff() || isStoreManager()) ) {
             return $query->where($employeeColumn, auth()->user()->employee->id);
         }
 
