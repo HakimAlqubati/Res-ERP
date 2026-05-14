f4ed0138e370ec98911fa9289ff16059bab43518
HakimAlqubati <hakimahmed123321@gmail.com>
2026-05-15 01:36:59 +0300

    Update EmployeeApplicationResource.php

--- app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource.php
+++ app/Filament/Clusters/HRApplicationsCluster/Resources/EmployeeApplicationResource.php
@@ -437,7 +437,7 @@ class EmployeeApplicationResource extends Resource
                         \Filament\Notifications\Notification::make()
                             ->danger()
                             ->title(__('lang.error') ?: 'Error')
-                            ->body(__('lang.payroll_already_processed') ?: 'Cannot process records for this employee in this period.')
+                            ->body('Cannot process records for this employee in this period.')
                             ->send();
 
                         DB::rollBack();
