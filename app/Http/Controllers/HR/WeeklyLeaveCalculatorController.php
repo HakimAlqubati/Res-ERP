07e9b23b4242dd9385d3da1ebe668577b58bc809
HakimAlqubati <hakimahmed123321@gmail.com>
2026-05-31 17:56:23 +0300

    Update WeeklyLeaveCalculatorController.php

--- app/Http/Controllers/HR/WeeklyLeaveCalculatorController.php
+++ app/Http/Controllers/HR/WeeklyLeaveCalculatorController.php
@@ -15,9 +15,15 @@ class WeeklyLeaveCalculatorController extends Controller
         if ($request->has('total_month_days') || $request->has('absent_days')) {
             $totalMonthDays = (int) $request->input('total_month_days', 30);
             $absentDays = (int) $request->input('absent_days', 0);
-
-            $calculator = new WeeklyLeaveCalculator();
-            $result = $calculator->calculate($totalMonthDays, $absentDays);
+            $isPeriodEnded = (bool) ($context['is_period_ended'] ?? true);
+            $isForPayroll = (bool) ($context['is_for_payroll'] ?? true);
+            $hasAutoLeave = (bool) ($context['has_auto_weekly_leave'] ?? true);
+            $calculator = new WeeklyLeaveCalculator;
+            $result = $calculator->calculate($totalMonthDays, $absentDays, [
+                'is_period_ended' => $isPeriodEnded,
+                'is_for_payroll' => $isForPayroll,
+                'has_auto_weekly_leave' => $hasAutoLeave,
+            ]);
         }
 
         return view('hr.weekly_leave_calculator', compact('result'));
