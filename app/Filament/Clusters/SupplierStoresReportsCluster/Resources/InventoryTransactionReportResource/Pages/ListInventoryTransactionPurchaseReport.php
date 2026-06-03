9425f7edfdc2c7369f7f3e090c4ba4399cf46802
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-03 21:46:57 +0300

    Update ListInventoryTransactionPurchaseReport.php

--- app/Filament/Clusters/SupplierStoresReportsCluster/Resources/InventoryTransactionReportResource/Pages/ListInventoryTransactionPurchaseReport.php
+++ app/Filament/Clusters/SupplierStoresReportsCluster/Resources/InventoryTransactionReportResource/Pages/ListInventoryTransactionPurchaseReport.php
@@ -23,7 +23,7 @@ class ListInventoryTransactionPurchaseReport extends ListRecords
         $reportService = new PurchaseInvoiceProductSummaryReportService();
         $filters       = [
             'product_id'       => $productId,
-            'group_by_invoice' => 0,
+            'group_by_invoice' => 1,
             'store_id'         => $storeId,
 
         ];
