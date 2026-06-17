843700d6401f96d1683cd3dda0c7f10536504475
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-17 23:07:07 +0300

    Update InventoryStockRepository.php

--- app/Modules/Stock/Reports/FifoBatchReports/Repositories/InventoryStockRepository.php
+++ app/Modules/Stock/Reports/FifoBatchReports/Repositories/InventoryStockRepository.php
@@ -22,7 +22,7 @@ final class InventoryStockRepository implements InventoryStockRepositoryInterfac
         // fromSub تُغلِّف الـ subquery كـ derived table محل CTE
         $query = DB::table($stockBatches, 'stock_batches')
             ->selectRaw('*, (total_in - total_out) AS current_stock')
-            ->selectRaw('((total_in - total_out)/in_package_size * unit_price) AS remaining_total_price')
+            ->selectRaw('((total_in - total_out) * unit_price) AS remaining_total_price')
             ->selectRaw('CASE WHEN ROW_NUMBER() OVER(PARTITION BY product_id ORDER BY id ASC) = 1 THEN true ELSE false END AS is_current_batch')
             ->selectRaw('CONCAT(REGEXP_REPLACE(SUBSTRING_INDEX(transactionable_type, "\\\\", -1), "([a-z])([A-Z])", "$1 $2"), " #", transactionable_id) AS source_document')
             ->whereRaw('(total_in - total_out) > 0');
@@ -56,8 +56,7 @@ final class InventoryStockRepository implements InventoryStockRepositoryInterfac
                 'in_t.product_id',
                 'p.name as product',
                 'u.name as unit',
-                'in_t.package_size as in_package_size',
-                // 'in_t.price as unit_price',
+                'in_t.package_size',
                 'in_t.transactionable_type',
                 'in_t.transactionable_id',
                 'in_t.movement_date',
