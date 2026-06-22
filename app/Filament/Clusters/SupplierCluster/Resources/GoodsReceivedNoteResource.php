342d4c31cb6ab4aab1df3eacfa22f2f4c9262bc3
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-22 23:08:51 +0300

    Update GoodsReceivedNoteResource.php

--- app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource.php
+++ app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource.php
@@ -381,6 +381,7 @@ class GoodsReceivedNoteResource extends Resource
                                         ->numeric()
                                         ->minValue(0.1)
                                         ->default(1)
+                                        ->maxLength(6)
                                         ->live(onBlur: true)
                                         ->afterStateUpdated(function ($set, $get) {
                                             $quantity = (float) ($get('quantity') ?? 0);
