04f2dd18d91a0061d79576d35e173ffa8570a655
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-22 21:58:59 +0300

    grn & uninvoiced grns report

--- routes/web.php
+++ routes/web.php
@@ -1123,3 +1123,6 @@ Route::post('secret-setup/store', [App\Http\Controllers\SecretSetupController::c
 Route::get('/login', function () {
     return redirect()->route('filament.admin.auth.login');
 })->name('login');
+
+Route::get('/reports/grn-count', [\App\Http\Controllers\GrnReportController::class, 'index'])
+    ->name('reports.grn-count')->middleware('web');
