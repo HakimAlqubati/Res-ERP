9e83ee806914c62e56d0d28e479465f107d1ef4a
HakimAlqubati <hakimahmed123321@gmail.com>
2026-06-28 00:00:13 +0300

    modify sort pages in payroll

--- app/Filament/Clusters/HRAttendanceReport/Resources/EmployeeAdvanceReportResource.php
+++ app/Filament/Clusters/HRAttendanceReport/Resources/EmployeeAdvanceReportResource.php
@@ -41,7 +41,7 @@ class EmployeeAdvanceReportResource extends Resource
     protected static ?string $pluralLabel = 'Staff Advances';
 
     protected static ?\Filament\Pages\Enums\SubNavigationPosition $subNavigationPosition = SubNavigationPosition::Top;
-    protected static ?int $navigationSort = 5;
+    protected static ?int $navigationSort = 2;
 
     public static function table(Table $table): Table
     {
