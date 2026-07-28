82c86d14e7fbc13bbb4f48bac825d72c37ad9646
HakimAlqubati <hakimahmed123321@gmail.com>
2026-07-29 01:27:21 +0300

    make action review and approve square

--- app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource.php
+++ app/Filament/Clusters/SupplierCluster/Resources/GoodsReceivedNoteResource.php
@@ -684,9 +684,9 @@ class GoodsReceivedNoteResource extends Resource
                         ->color('green') ->hidden(),
                 ]),
                 Action::make('PreviewAndApprove')
-                    ->label('Review and Approve')
+                    ->label(new \Illuminate\Support\HtmlString('Review & <br> Approve'))
                     ->color('primary')
-                    ->icon('heroicon-o-clipboard-document-check')
+                    // ->icon('heroicon-o-clipboard-document-check')
                     ->url(fn($record) => static::getUrl('create-purchase-invoice', ['record' => $record]))
                     ->button()
                     ->visible(function ($record) {
