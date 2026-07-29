4d0bd830113ab92e232c978591ff7ec846358f40
HakimAlqubati <hakimahmed123321@gmail.com>
2026-07-30 01:45:51 +0300

    stocktake validator for processing orders , unaudited stocktake  branch only

--- app/Filament/Clusters/SupplierStoresReportsCluster/Resources/StockInventoryResource/Pages/ListStockInventories.php
+++ app/Filament/Clusters/SupplierStoresReportsCluster/Resources/StockInventoryResource/Pages/ListStockInventories.php
@@ -48,7 +48,7 @@ class ListStockInventories extends ListRecords
                         // ->default(8)
                         ->options(fn() => Store::active()
                             ->whereHas('branches', function ($query) {
-                                $query->whereIn('type', [Branch::TYPE_BRANCH,Branch::TYPE_CENTRAL_KITCHEN]);
+                                $query->whereIn('type', [Branch::TYPE_BRANCH]);
                             })
                             ->pluck('name', 'id'))
                         ->required()
